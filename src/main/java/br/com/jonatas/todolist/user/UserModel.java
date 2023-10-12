6986ecfb37294687dd7e0f6fff58e923e70073eb
jonatas.almeida <jonatas.almeida@globalhitss.com.br>
2023-10-11 22:56:14 -0300

    first commit

---
+++ src/main/java/br/com/jonatas/todolist/user/UserModel.java
@@ -0,0 +1,18 @@
+package br.com.jonatas.todolist.user;
+
+public class UserModel {
+    
+    /**
+     * caso eu não utilize um modificador
+     * por padrão ele recebe public
+     * 
+     * CUIDADO
+     * ao não colocar public
+     * na primeira vez que tentei um post
+     *  retornou null
+     */
+
+    public String name;
+    public String username;
+    public String password;
+}
