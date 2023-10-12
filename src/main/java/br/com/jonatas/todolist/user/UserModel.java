3ad132f18c5010e5ffb2cec134202e99cbf3cdc9
jonatas.almeida <jonatas.almeida@globalhitss.com.br>
2023-10-12 00:32:32 -0300

    Ciração da interface com regras de uso do UserModel Utilização do ResponseEntity para retorno de httpStatus Uso de decorators p/ criação única de algumas colunas no db

--- src/main/java/br/com/jonatas/todolist/user/UserModel.java
+++ src/main/java/br/com/jonatas/todolist/user/UserModel.java
@@ -1,5 +1,14 @@
 package br.com.jonatas.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 /**
@@ -9,6 +18,7 @@ import lombok.Data;
  */
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
     
     /**
@@ -25,9 +35,18 @@ public class UserModel {
     // public String username;
     // public String password;
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String name;
+    
+    @Column(unique = true)
     private String username;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 
     /**
